5ac28a05ec73fe1742558010136f1dbf0b14f2a4
Hema Devi <uhemadevi1927@gmail.com>
2024-01-30 09:40:08 +0530

    Add DecentralizedStableCoin contract

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.18;
+
+/*@title decentralizedStableCoin
+*@author HemaDeviU
+Collateral: Exogenous (ETH &BTC)
+Minting : Alogirthemic
+Relatcie Stability :Pegged to USD
+*This contract is meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoinsystem
+*/
+contract DecentralizedStableCoin {
+    constructor() {}
+}
